ba2a1a0c5c1ce40987a00eac1e83ef4cd844766b
Azzuro <Azzuro@users.noreply.github.com>
2014-12-22 21:05:18 +0100

    rename the zip name.

--- mediaportal/WatchDog/MPWatchDog.cs
+++ mediaportal/WatchDog/MPWatchDog.cs
@@ -197,12 +197,12 @@ namespace WatchDog
       
       if (_watchdogtargetDir == string.Empty)
       {
-        zipFile = string.Format("{0}\\MediaPortal-Logs\\{1}_MediaPortalLogs_[date]__[time].zip",
+        zipFile = string.Format("{0}\\MediaPortal-Logs\\MP_logs__{1}__[date]__[time].zip",
           Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Environment.MachineName);
       }
       else
       {
-        zipFile = string.Format("{0}\\{1}_MediaPortalLogs_[date]__[time].zip", _watchdogtargetDir, Environment.MachineName);
+        zipFile = string.Format("{0}\\MP_logs__{1}__[date]__[time].zip", _watchdogtargetDir, Environment.MachineName);
       }
       
       if (!ParseCommandLine())
@@ -647,7 +647,7 @@ namespace WatchDog
 
     private void btnZipFileReset_Click(object sender, EventArgs e)
     {
-      zipFile = string.Format("{0}\\MediaPortal-Logs\\{1}_MediaPortalLogs_[date]__[time].zip", 
+      zipFile = string.Format("{0}\\MediaPortal-Logs\\MP_logs__{1}__[date]__[time].zip", 
         Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Environment.MachineName);
       tbZipFile.Text = zipFile;
     }
